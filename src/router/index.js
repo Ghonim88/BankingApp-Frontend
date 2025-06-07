0470f3db0082b3d7ae918aa1fe673fef7f9e5e33
Abdelrahman Ghonim <bodyghonim345@gmail.com>
2025-06-07 22:40:24 +0200

    Hosting forntend

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,8 @@ import AccountDetails from "@/components/employeePanel/AccountDetails.vue";
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  //history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/BankingApp-Frontend/'),
   routes: [
     // Default routes
   { path: "/",redirect: "/login" },
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'BankingApp-Frontend/',
+  base: '/BankingApp-Frontend/',
   plugins: [
     vue(),
     vueDevTools(),
