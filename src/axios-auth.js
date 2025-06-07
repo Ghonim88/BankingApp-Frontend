4b4447657bbabce2cba0b7dcdb1515b07b076c10
Abdelrahman Ghonim <bodyghonim345@gmail.com>
2025-06-07 14:55:37 +0200

    fixing axios

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
- // baseURL: "http://localhost:8080/",
-   baseURL: import.meta.env.VITE_API_BASE_URL,
+ baseURL: "http://localhost:8080/",
+ //  baseURL: import.meta.env.VITE_API_BASE_URL,
 
 });
 
